e72d1e000012eb8a5513b95edd485771f89bd489
Chris Tomlinson <ct@moonvine.org>
2016-05-21 20:03:13 -0500

    Update provision.sh install node-couch so that node.js can i/f with couchdb

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -37,6 +37,9 @@ echo ">>>> installing node.js; npm; and n"
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g n
+# install node.js i/f to couchdb - 
+# see https://www.npmjs.com/package/node-couch
+sudo npm install node-couch --save
 # to update node.js use
 # sudo npm cache clean -f
 # sudo n stable
