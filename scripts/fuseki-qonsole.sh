3773ef6aa0b226314c7a95464b62c76603e30f2e
Chris Tomlinson <ct@moonvine.org>
2017-03-23 09:46:17 -0500

    correct typo qonsole

--- scripts/fuseki-qonsole.sh
+++ scripts/fuseki-qonsole.sh
@@ -6,6 +6,6 @@ export THE_HOME=/usr/local/$SVC
 systemctl stop $SVC
 echo ">>>> patching ${SVC} query console configuration"
 # patching ${SVC} query console configuration and restart
-cp /vagrant/conf/fuseki/qconsole-config.js $THE_HOME/tomcat/webapps/fuseki/js/app/
+cp /vagrant/conf/fuseki/qonsole-config.js $THE_HOME/tomcat/webapps/fuseki/js/app/
 systemctl start $SVC
 echo ">>>> ${SVC} service listening on ${MAIN_PORT}"
