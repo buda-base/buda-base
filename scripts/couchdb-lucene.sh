9225485f13d732a14223c6c79e492d648753cb33
Chris Tomlinson <ct@moonvine.org>
2017-04-24 14:52:01 -0500

    change release to include latest generic analyzer extension

--- scripts/couchdb-lucene.sh
+++ scripts/couchdb-lucene.sh
@@ -2,13 +2,15 @@
 
 echo ">>>> installing couchdb-lucene."
 
+export REL="https://github.com/BuddhistDigitalResourceCenter/couchdb-lucene/releases/download/v2.1.0_bdrc/couchdb-lucene-2.1.0_bdrc-dist.tar.gz"
+
 if [ -a /usr/local/couchdb-lucene/bin/run ]; then
    	echo ">>>> couchdb-lucene present. Skipping couchdb-lucene download."
 else
    	echo ">>>> downloading couchdb-lucene."
 	cd /usr/local/
 	mkdir couchdb-lucene
-	curl -fSLs https://github.com/BuddhistDigitalResourceCenter/couchdb-lucene/releases/download/v2.2.0pre/couchdb-lucene-2.2.0-SNAPSHOT-dist.tar.gz -o couchdb-lucene.tar.gz
+	curl -fSLs $REL -o couchdb-lucene.tar.gz
 	tar -xzf couchdb-lucene.tar.gz -C couchdb-lucene --strip-components=1
 	rm couchdb-lucene.tar.gz
 fi
