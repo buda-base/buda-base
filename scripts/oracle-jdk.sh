e13842e2c97a4ce34c679b00c058c2bd4c1f3d7e
Chris Tomlinson <ct@moonvine.org>
2017-04-24 14:53:46 -0500

    don't skip jdk 8 install just because there's some java already installed

--- scripts/oracle-jdk.sh
+++ scripts/oracle-jdk.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if which java >/dev/null; then
-   	echo "skip java 8 installation"
-else
+#if which java >/dev/null; then
+#   	echo "skip java 8 installation"
+#else
 	echo "java 8 installation"
 	echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
 	echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
@@ -12,4 +12,4 @@ else
 	echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections
 	apt-get install --yes oracle-java8-installer oracle-java8-set-default
 	yes "" | apt-get -f install
-fi
+#fi
