b03e525c6e6ff4591925df1cf504393d5f140fac
Chris Tomlinson <ct@moonvine.org>
2018-01-29 11:43:56 -0600

    add nginx to map port 80 to ldspdi

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+echo ">>>> Installing Nginx and Openssl"
+
 apt-get install nginx openssl
 
 if [ ! -f /etc/nginx/dhparam.pem ]; then
@@ -5,3 +9,14 @@ if [ ! -f /etc/nginx/dhparam.pem ]; then
 fi
 
 cp /vagrant/conf/nginx/ssl_params > /etc/nginx/ssl_params
+
+echo ">>>> set up default config: port 80 to ldspdi
+# rm the link to /etc/nginx/sites-available/default
+rm /etc/nginx/sites-enabled/default
+cp /vagrant/conf/nginx/default /etc/nginx/sites-enabled/
+
+sudo systemctl enable nginx
+
+sudo systemctl restart nginx
+
+echo ">>>> Nginx Installed"
