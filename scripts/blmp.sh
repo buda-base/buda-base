aea67b476f7e32e6fe607d76e3052fe9a1d1796c
Chris Tomlinson <ct@moonvine.org>
2018-02-01 12:31:54 -0600

    added test for ON_AWS to control local install vs nginx

--- scripts/blmp.sh
+++ scripts/blmp.sh
@@ -3,7 +3,12 @@ echo ">>>> Installing blmp..."
 export SVC_DESC=" BUDA Library Management Portal"
 export TC_USER=blmp
 export TC_GROUP=blmp
-export DATA_DIR=/mnt/data ;
+if [ -d /mnt/data ] ; then 
+  export DATA_DIR=/mnt/data ; 
+else
+  export DATA_DIR=/usr/local ;
+  export ON_AWS=yes ;
+fi
 
 echo ">>>> DATA_DIR: " $DATA_DIR
 export DOWNLOADS=$DATA_DIR/downloads
@@ -20,11 +25,13 @@ echo ">>>> downloading owl-schema"
 git clone https://github.com/BuddhistDigitalResourceCenter/owl-schema.git
 
 echo ">>>> downloading & installing blmp-prototype_flow"
-git clone https://github.com/BuddhistDigitalResourceCenter/blmp-prototype-flow.git
-ls -l blmp* owl*
-cd blmp-prototype-flow
-ls -l public
-yarn install
-yarn build
-# yarn install
-# yarn build
+if $ON_AWS then
+    echo ">>>> should be installing blmp in nginx" ;
+else
+	git clone https://github.com/BuddhistDigitalResourceCenter/blmp-prototype-flow.git
+	ls -l blmp* owl*
+	cd blmp-prototype-flow
+	ls -l public
+	yarn install
+	yarn build
+fi
