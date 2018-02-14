9476da2f10b49fca3c5fc8d09a73cf3a4442aef3
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-02-14 09:45:17 +0100

    the usual cleanup

--- scripts/cantaloupe.sh
+++ scripts/cantaloupe.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo ">>>> Installing cantaloupe IIIF server..."
-export SVC_DESC="BUDA Image Service - IIIF compliant"
+export SVC_DESC="Cantaloupe"
 export TC_USER=cantaloupe
 export TC_GROUP=cantaloupe
 
@@ -15,7 +15,6 @@ export DOWNLOADS=$DATA_DIR/downloads
 export JAVA_HOME=`type -p javac|xargs readlink -f|xargs dirname|xargs dirname`
 export GEM_HOME=$DATA_DIR/ruby
 export CANTALOUPE=cantaloupe
-export CANTALOUPE_HOST=localhost
 export CANTALOUPE_HOME=$DATA_DIR/$CANTALOUPE/jetty
 export CANTALOUPE_WEBAPPS=$CANTALOUPE_HOME/webapps
 export CANTALOUPE_GIT=$DATA_DIR/downloads/cantaloupe-bdrc
@@ -31,35 +30,37 @@ useradd -s /bin/bash -g $TC_GROUP -d $CANTALOUPE_HOME $TC_USER
 echo ">>>> installing jetty 9 for cantaloupe"
 
 mkdir $DOWNLOADS
-mkdir $DATA_DIR/$CANTALOUPE
-mkdir $CANTALOUPE_HOME
+mkdir -p $DATA_DIR/$CANTALOUPE
+mkdir -p $CANTALOUPE_HOME
 
-# Installing gem
-echo ">>>> Installing gem..."
-apt-get install gem
 
-echo ">>>> Installing dalli gem..."
+if [ $(dpkg-query -W -f='${Status}' gem 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+	apt-get install gem memcached -y
+fi
+
+echo ">>>> Installing gems"
 gem install --install-dir $GEM_HOME dalli
 
-cd $DOWNLOADS
-wget "http://central.maven.org/maven2/org/eclipse/jetty/jetty-distribution/9.4.8.v20171121/jetty-distribution-9.4.8.v20171121.tar.gz"
-tar xf $DOWNLOADS/jetty-distribution-9*tar.gz -C $CANTALOUPE_HOME --strip-components=1
+pushd $DOWNLOADS
+wget -q -c "http://central.maven.org/maven2/org/eclipse/jetty/jetty-distribution/9.4.8.v20171121/jetty-distribution-9.4.8.v20171121.tar.gz"
+tar xf jetty-distribution-9.4.8.v20171121.tar.gz -C $CANTALOUPE_HOME --strip-components=1
 
 # configure server
 echo ">>>> configuring jetty 9 for deployment"
-erb /vagrant/conf/jetty/cantaloupe-app.xml.erb > $CANTALOUPE_WEBAPPS/cantaloupe-app.xml
+erb /vagrant/conf/cantaloupe/cantaloupe-app.xml.erb > $CANTALOUPE_WEBAPPS/cantaloupe-app.xml
 
-echo ">>>> getting Cantaloupe-3.7.1..."
-wget 'https://github.com/medusa-project/cantaloupe/releases/download/v3.4.1/Cantaloupe-3.4.1.zip'
+echo ">>>> getting Cantaloupe..."
+wget -q -c 'https://github.com/medusa-project/cantaloupe/releases/download/v3.4.1/Cantaloupe-3.4.1.zip'
 unzip Cantaloupe-3.4.1.zip
-mv $DOWNLOADS/Cantaloupe-3.4.1.war $CANTALOUPE_HOME/cantaloupe.war
+mv Cantaloupe-3.4.1/Cantaloupe-3.4.1.war $CANTALOUPE_HOME/cantaloupe.war
+popd
 
 echo ">>>> getting Cantaloupe script..."
-wget 'https://raw.githubusercontent.com/BuddhistDigitalResourceCenter/cantaloupe-scripts/master/ruby/delegates.rb'
+wget -q -c 'https://raw.githubusercontent.com/BuddhistDigitalResourceCenter/cantaloupe-scripts/master/ruby/delegates.rb'
 cp delegates.rb $CANTALOUPE_HOME
-erb /vagrant/conf/jetty/startup.sh.erb > $CANTALOUPE_HOME/startup.sh
-erb /vagrant/conf/jetty/shutdown.sh.erb > $CANTALOUPE_HOME/shutdown.sh
-
+erb /vagrant/conf/cantaloupe/startup.sh.erb > $CANTALOUPE_HOME/startup.sh
+erb /vagrant/conf/cantaloupe/shutdown.sh.erb > $CANTALOUPE_HOME/shutdown.sh
 
 echo ">>>> fixing permissions"
 chown -R $USER:$USER $DOWNLOADS
@@ -67,10 +68,9 @@ chown -R $TC_USER:$TC_GROUP $CANTALOUPE_HOME
 
 # setup as Debian systemctl service listening on 9099
 echo ">>>> setting up ${CANTALOUPE} as service listening on 9099"
-erb /vagrant/conf/jetty/systemd.erb > /etc/systemd/system/$CANTALOUPE.service
-rm -r $DOWNLOADS
+erb /vagrant/conf/cantaloupe/systemd.erb > /etc/systemd/system/$CANTALOUPE.service
 
 echo ">>>> starting ${CANTALOUPE} service"
 systemctl daemon-reload
 systemctl enable $CANTALOUPE
-systemctl start $CANTALOUPE &
+systemctl start $CANTALOUPE
--- scripts/ruby.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# install ruby
-sudo apt-get install ruby -y
