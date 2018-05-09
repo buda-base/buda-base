0c2d1578a711f4027f6cc2188fcbb57ee7526624
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-05-09 18:45:43 +0200

    small fixes in update

--- conf/iiifserv/update.sh
+++ conf/iiifserv/update.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-
-
 echo ">>>> Updating iiif server"
 export DATA_DIR=/usr/local ;
 export TC_USER=iiifserv
@@ -19,20 +17,20 @@ mkdir -p $THE_HOME
 
 # install iiifserv
 echo ">>>> installing iiif server"
-cd $DOWNLOADS;
+pushd $DOWNLOADS;
 git clone https://github.com/BuddhistDigitalResourceCenter/buda-iiif-server.git
 cd buda-iiif-server
 if [ "$#" -eq 1 ]; then
     git checkout "$1"
 fi
-mvn package
+mvn clean package
 
 chown $TC_USER:$TC_GROUP target/*.jar
-cp target/buda-hymir-1.0.0-SNAPSHOT-exec.jar $THE_HOME/buda-hymir-exec.jar
+cp target/*-exec.jar $THE_HOME/buda-hymir-exec.jar
 
-cd /
-rm -rf $DOWNLOADS/$IIIFSERV
+popd
+rm -rf --preserve-root $DOWNLOADS/$IIIFSERV
 
-service iiifserv stop
+service iiifserv start
 
 echo ">>>> iiifserv was updated"
