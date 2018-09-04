acbf87c2763edd4254cb89e6a4be6efcf621c6d3
Chris Tomlinson <ct@moonvine.org>
2018-09-04 15:20:15 -0500

    additional prefixes

--- conf/fuseki/qonsole-config.js
+++ conf/fuseki/qonsole-config.js
@@ -3,19 +3,23 @@
 define( [], function() {
   return {
     prefixes: {
-      "" : "http://purl.bdrc.io/ontology/core/",
-      "adm" : "http://purl.bdrc.io/ontology/admin/",
-      "bdd" : "http://purl.bdrc.io/data/",
-      "bdo" : "http://purl.bdrc.io/ontology/core/",
-      "bdr" : "http://purl.bdrc.io/resource/",
-      "owl" : "http://www.w3.org/2002/07/owl#",
-      "rdf" : "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-      "rdfs" : "http://www.w3.org/2000/01/rdf-schema#",
-      "skos" : "http://www.w3.org/2004/02/skos/core#",
-      "tbr" : "http://purl.bdrc.io/ontology/toberemoved/",
-      "text" : "http://jena.apache.org/text#",
-      "vcard" : "http://www.w3.org/2006/vcard/ns#",
-      "xsd" : "http://www.w3.org/2001/XMLSchema#"
+      "rdf"   : "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+      "rdfs"  : "http://www.w3.org/2000/01/rdf-schema#",
+      "owl"   : "http://www.w3.org/2002/07/owl#",
+      "xsd"   : "http://www.w3.org/2001/XMLSchema#",
+      ""      : "http://purl.bdrc.io/ontology/core/",
+      "adm"   : "http://purl.bdrc.io/ontology/admin/",
+      "bdd"   : "http://purl.bdrc.io/data/",
+      "bdo"   : "http://purl.bdrc.io/ontology/core/",
+      "bdr"   : "http://purl.bdrc.io/resource/",
+      "aut"   : "http://purl.bdrc.io/ontology/ext/auth/",
+      "adr"   : "http://purl.bdrc.io/resource-auth/",
+      "oa"	  : "http://www.w3.org/ns/oa#",
+      "foaf"  : "http://xmlns.com/foaf/0.1/",
+      "skos"  : "http://www.w3.org/2004/02/skos/core#",
+      "tbr"   : "http://purl.bdrc.io/ontology/toberemoved/",
+      "text"  : "http://jena.apache.org/text#",
+      "vcard" : "http://www.w3.org/2006/vcard/ns#"
     },
     queries: [
       { "name": "Selection of triples",
