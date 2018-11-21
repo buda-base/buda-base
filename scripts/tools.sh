df5c26b257de7bf15a2f34e6612a22d830b68f9f
Chris Tomlinson <ct@moonvine.org>
2018-11-21 11:32:43 -0600

    use apt-get install on a single item at  time

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -1,15 +1,48 @@
 #!/usr/bin/env bash
 
+echo ">>>> update packages"
+apt-get update -y
 echo ">>>> upgrade packages"
-apt update -y
-apt upgrade -y
+apt-get upgrade -y
 
 #install common tools and utilities
 echo ">>>> installing common tools and utilities"
-echo ">>>> installing python setup tools"
-apt-get install python-dev libxml2-dev libxslt-dev libyaml-dev python-setuptools python-pip python3-pip lsof -y -q
+echo ">>>> installing python-dev"
+apt-get install python-dev  -y -q
+echo ">>>> installing libxml2-dev"
+apt-get install libxml2-dev  -y -q
+echo ">>>> installing libxslt-dev"
+apt-get install libxslt-dev  -y -q
+echo ">>>> installing libyaml-dev"
+apt-get install libyaml-dev  -y -q
+echo ">>>> installing python-setuptools"
+apt-get install python-setuptools  -y -q
+echo ">>>> installing python-pip"
+apt-get install python-pip  -y -q
+echo ">>>> installing python3-pip"
+apt-get install python3-pip  -y -q
+echo ">>>> installing lsof"
+apt-get install lsof -y -q
+echo ">>>> pip3 install sparqlwrapper"
 pip3 install sparqlwrapper -q
-echo ">>>> installing various common tools"
-# ruby needed for erb
-apt-get install tree git bash-completion sudo vim curl jq wget ruby maven -y -q
+echo ">>>> installing tree"
+apt-get install tree -y -q
+echo ">>>> installing git"
+apt-get install git -y -q
+echo ">>>> installing bash-completion"
+apt-get install bash-completion -y -q
+echo ">>>> installing sudo"
+apt-get install sudo -y -q
+echo ">>>> installing vim"
+apt-get install vim -y -q
+echo ">>>> installing curl"
+apt-get install curl -y -q
+echo ">>>> installing jq"
+apt-get install jq -y -q
+echo ">>>> installing wget"
+apt-get install wget -y -q
+echo ">>>> installing ruby"
+apt-get install ruby -y -q
+echo ">>>> installing maven"
+apt-get install maven -y -q
 echo ">>>> common tools and utilities installed"
