00634ac6191996654de3ad6f272411e7ac3f228e
MarcAgate <agate.marc@gmail.com>
2018-12-03 19:47:57 +0100

    updated config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,5 +1,15 @@
+if [ -d /mnt/data ] ; then 
+  export DATA_DIR=/mnt/data ;
+else
+  export DATA_DIR=/usr/local ;
+fi
+echo ">>>> DATA_DIR: " $DATA_DIR
+export DOWNLOADS=$DATA_DIR/downloads
+mkdir -p /etc/buda/share/geolite/
+cd $DOWNLOADS
 echo ">>>> setting up Geolite DB"
 wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz
 tar -xvzf GeoLite2-Country.tar.gz
 cp */GeoLite2-Country.mmdb /etc/buda/share/geolite/GeoLite2-Country.mmdb
+chmod 600 /etc/buda/share/geolite/GeoLite2-Country.mmdb 
 
