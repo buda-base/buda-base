23bbc1936c5c372bea4f0bb5f465112665b7cca1
MarcAgate <agate.marc@gmail.com>
2018-12-03 20:09:03 +0100

    put all services config in config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -13,3 +13,30 @@ tar -xvzf GeoLite2-Country.tar.gz
 cp */GeoLite2-Country.mmdb /etc/buda/share/geolite/GeoLite2-Country.mmdb
 chmod 600 /etc/buda/share/geolite/GeoLite2-Country.mmdb 
 
+#Config ldspdi
+echo ">>>> setting up ldspdi config"
+mkdir -p /etc/buda/ldspdi
+cp /vagrant/conf/etc/buda/ldspdi/ldspdi-private.properties /etc/buda/ldspdi/
+chown ldspdi:ldspdi /etc/buda/ldspdi/ldspdi-private.properties
+
+#Config iiifserv
+echo ">>>> setting up iiif server config"
+mkdir -p /etc/buda/iiifserv
+cp /vagrant/conf/etc/buda/iiifserv/application.yml /etc/buda/iiifserv/
+cp /vagrant/conf/etc/buda/iiifserv/credentials /etc/buda/iiifserv/
+cp /vagrant/conf/etc/buda/iiifserv/iiifserv-private.properties /etc/buda/iiifserv/
+
+chown iiifserv:iiifserv /etc/buda/iiifserv/application.yml 
+chown iiifserv:iiifserv /etc/buda/iiifserv/credentials
+chown iiifserv:iiifserv /etc/buda/iiifserv/iiifserv-private.properties
+
+#Config iiifpres
+echo ">>>> setting up iiif presentation config"
+mkdir -p /etc/buda/iiifpres
+cp /vagrant/conf/etc/buda/iiifpres/credentials /etc/buda/iiifpres/
+cp /vagrant/conf/etc/buda/iiifpres/iiifpres-private.properties /etc/buda/iiifpres/
+ 
+chown iiifpres:iiifpres /etc/buda/iiifpres/credentials
+chown iiifpres:iiifpres /etc/buda/iiifpres/iiifpres-private.properties
+
+echo ">>>> Buda services config was set"
