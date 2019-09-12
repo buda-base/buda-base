15f0acb34c86ba0a7c0a99f81e56cf852c7b5276
Marc Agate <agate.marc@gmail.com>
2019-09-12 09:06:12 -0400

    Update iiifpres setup config

--- conf/iiifpres/update.sh
+++ conf/iiifpres/update.sh
@@ -1,36 +1,43 @@
 #!/usr/bin/env bash
 
-echo ">>>> installing iiifpres"
-
+echo ">>>> Updating iiifpres server"
 if [ -d /mnt/data ] ; then 
   export DATA_DIR=/mnt/data ; 
 else
   export DATA_DIR=/usr/local ;
 fi
-
+export TC_USER=iiifpres
+export TC_GROUP=iiifpres
+export SVC=iiifpres
 export DOWNLOADS="$DATA_DIR/downloads"
-export IIIFPRES="iiifpres"
-export IIIFPRES_HOME="$DATA_DIR/$IIIFPRES"
+export IIIFSERV="iiifpres"
+export THE_HOME=$DATA_DIR/$SVC
+export AWS_CREDENTIAL_PROFILES_FILE=/etc/buda/iiifpres/credentials
 
 service iiifpres stop
 
 echo ">>>> cloning iiifpres"
-mkdir -p $DOWNLOADS/$IIIFPRES
-pushd $DOWNLOADS/$IIIFPRES
+mkdir -p $DOWNLOADS
+mkdir -p $THE_HOME
 
-rm -rf --preserve-root tmpgitiiifpresentation
-git clone https://github.com/BuddhistDigitalResourceCenter/buda-iiif-presentation.git tmpgitiiifpresentation
-pushd tmpgitiiifpresentation
+# install iiifpres
+echo ">>>> installing iiifpres server"
+rm -rf --preserve-root $DOWNLOADS/buda-iiif-presentation
+pushd $DOWNLOADS;
+git clone https://github.com/BuddhistDigitalResourceCenter/buda-iiif-presentation.git
+cd buda-iiif-presentation
 if [ "$#" -eq 1 ]; then
     git checkout "$1"
 fi
-mvn -B clean package
-chown iiifpres:iiifpres target/*.war
-rm -rf --preserve-root $IIIFPRES_HOME/tomcat/webapps/ROOT
-mv target/*.war $IIIFPRES_HOME/tomcat/webapps/ROOT.war
+
+mvn -B clean package -Diiifpres.configpath=/etc/buda/iiifpres/ -Dspring.config.location=file:/etc/buda/iiifpres/application.yml
+
+chown $TC_USER:$TC_GROUP target/*.war
+cp target/*-exec.war $THE_HOME/buda-iiif-presentation-exec.war
+
 popd
-rm -rf --preserve-root tmpgitiiifpresentation
+#rm -rf --preserve-root $DOWNLOADS/$IIIFSERV
 
 service iiifpres start
 
-echo ">>>> iiifpres installed"
+echo ">>>> iiifpres was updated"
