64588496b71c5f5d866ba4e47888ad6bd95a3b75
eroux <elie.roux@telecom-bretagne.eu>
2020-07-22 09:59:29 +0200

    add extended test

--- tests/run-sparql-test.py
+++ tests/run-sparql-test.py
@@ -72,7 +72,7 @@ def run_testgroup(testgroupname, specifictest=None):
     #run_pre_post(testgroupname, "post")
 
 if __name__ == '__main__':
-    run_testgroup("lucene-zh")
+    run_testgroup("extended")
     #run_testgroup("lucene-sa", "test1")
     #run_testgroup("lucene-sa", "test3")
     #run_testgroup("lucene-bo")
